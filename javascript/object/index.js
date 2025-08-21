aead62b33a6d5e660ebdf6b09e13f6b17232d6f8
vikasmauryaofficial <officialvikasmaurya@gamil.com>
2025-08-21 11:11:29 +0530

    javascript3

--- javascript/object/index.js
+++ javascript/object/index.js
@@ -3,19 +3,24 @@ const employee={
     eage: 30,
     edesignation: 'developer',
     esalary: 50000,  
-    planguage: {
+    planguage:[{
         pl1: 'java',
         pl2: 'javascript',
         pl3: 'python'
-    },
-    databases: {
+    }],
+    databases: [{
         database1: 'mysql',
         database2: 'mongodb',
         database3: 'postgresql'
-    },framework:{
+    }],framework:[{
         framework1: 'spring',
         framework2: 'react',
         framework3: 'angular'
-    }
+    }]
 }
-console.log(employee);
+
+employee.company = 'Tech Solutions';
+console.log(employee); // Output: 'java'
+
+employee.planguage[0].pl1 = 'typescript';
+console.log(employee.planguage[0].pl1); // Output: 'typescript'
