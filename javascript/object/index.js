90a078da3907174c1bad1ab95175b12138fafafa
vikasmauryaofficial <officialvikasmaurya@gamil.com>
2025-08-21 10:59:43 +0530

    javascript

---
+++ javascript/object/index.js
@@ -0,0 +1,12 @@
+const employee={
+    ename: 'mohan', 
+    eage: 30,
+    edesignation: 'developer',
+    esalary: 50000,  
+    planguage:{
+        pl1: 'java',
+        pl2: 'javascript',
+        pl3: 'python'   
+    }   
+}
+console.log(employee);
